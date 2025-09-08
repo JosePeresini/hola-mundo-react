76d21a43a9312c0e21a2f44c05f7cf0e82ee987d
Jose Peresini <joseignacioperesini@gmail.com>
2025-09-07 23:40:00 -0300

    add products

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import { Component } from "react";
 
 class App extends Component {
+  state = {
+    productos: [
+      { name: "Tomate", price: 1500, img: "/tomate.png" },
+      { name: "Arbeja", price: 1500, img: "/arbeja.png" },
+      { name: "Lechuga", price: 1500, img: "/lechuga.png" },
+    ],
+  };
+
   render() {
     return (
       <>
