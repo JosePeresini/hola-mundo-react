f2932ec083e0c519fd41097127a198be3453a70e
Jose Peresini <joseignacioperesini@gmail.com>
2025-09-07 23:33:32 -0300

    start project class

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <div>Hola Mundo</div>
+      </>
+    );
+  }
+}
+
 export default App;
